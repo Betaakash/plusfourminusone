79c787256d0c1606c886485aa21f3f71c8f88b48
Aakash <aakash@sharma.com>
2023-06-26 12:29:17 +0530

    added +4, -1, Undo, Reset Button

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const App = () => {
+  const [count, setCount] = useState(0);
+  const [history, setHistory] = useState([]);
+
+  const handleAdd = () => {
+    const updatedCount = count + 4;
+    setCount(updatedCount);
+    setHistory([...history, count]);
+    // checkTotalSum(updatedCount);
+  };
+
+  const handleSubtract = () => {
+    const updatedCount = count - 1;
+    setCount(updatedCount);
+    setHistory([...history, count]);
+    // checkTotalSum(updatedCount);
+  };
+
+  const handleUndo = () => {
+    if (history.length > 0) {
+      const previousCount = history[history.length - 1];
+      setCount(previousCount);
+      setHistory(history.slice(0, history.length - 1));
+      // checkTotalSum(previousCount);
+    }
+  };
+
+  const handleReset = () => {
+    setCount(0);
+    setHistory([]);
+  };
+
+  
+  // const checkTotalSum = (total) => {
+  //   if (total >= 150 && total < 200) {
+  //     alert("Great!! You crossed 150");
+  //   } else if (total >= 200 && total < 250) {
+  //     alert("Emhm! Now we are talking");
+  //   } else if (total >= 250) {
+  //     alert("Woah! Legend.");
+  //   }
+  // };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='containerr'>
+      <h1 id="sum">Total Sum: {count}</h1>
+      <button onClick={handleAdd}>+4</button>
+      <button onClick={handleSubtract}>-1</button>
+      <br />
+      <button onClick={handleUndo}>Undo</button>
+      <button onClick={handleReset}>Reset</button>
     </div>
   );
-}
+};
 
 export default App;
+
+
